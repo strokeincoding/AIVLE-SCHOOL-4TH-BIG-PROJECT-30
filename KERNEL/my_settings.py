9b72ade2ccb6cb2ad61f1de49c9df93e14bc1c21
sunbeen0111@naver.com <sunbeen0111@naver.com>
2023-12-19 03:39:27 +0900

    dbtest db접근

--- KERNEL/my_settings.py
+++ KERNEL/my_settings.py
@@ -1,10 +1,10 @@
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # 사용할 데이터베이스 엔진
-        'NAME': 'signup', # 데이터베이스 이름 
-        'USER': 'aivle', # 접속할 Database 계정 아이디 ex) root
-        'PASSWORD': 'aivle',  # 접속할 Database 계정 비밀번호 ex) 1234
-        'HOST': 'localhost',   # host는 로컬 환경에서 동작한다면 ex) localhost 192.168.56.1
+        'NAME': 'dbtest', # 데이터베이스 이름 
+        'USER': 'newuser', # 접속할 Database 계정 아이디 ex) root
+        'PASSWORD': '1234',  # 접속할 Database 계정 비밀번호 ex) 1234
+        'HOST': '192.168.56.1',   # host는 로컬 환경에서 동작한다면 ex) localhost 192.168.56.1
         'PORT': '3306', # 설치시 설정한 port 번호를 입력한다. ex) 3306ㅇㅇ
     }
 }
