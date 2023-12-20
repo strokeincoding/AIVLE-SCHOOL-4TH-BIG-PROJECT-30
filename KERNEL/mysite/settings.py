b9c04a2364b5dfc87e426098d233ae207b1f1948
kimtaehyeong <steve181813@gmail.com>
2023-12-20 15:05:37 +0900

    change db

--- KERNEL/mysite/settings.py
+++ KERNEL/mysite/settings.py
@@ -74,12 +74,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = my_settings.DATABASES
 
 
 # Password validation
