d45545782e700ac7d77da2ef2c9a1345f1ec4c1c
kimtaehyeong <steve181813@gmail.com>
2023-12-20 15:02:06 +0900

    ignore my_settings

---
+++ KERNEL/my_settings.py
@@ -0,0 +1,10 @@
+DATABASES = { 
+	'default': { 
+    	'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'aivlebigproject', 
+        'USER': 'root', 
+        'PASSWORD': '990813', 
+        'HOST': '127.0.0.1', 
+        'PORT': '3306', 
+     } 
+}
--- KERNEL/mysite/settings.py
+++ KERNEL/mysite/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-
+import my_settings
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -73,12 +73,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = my_settings.DATABASES
 
 
 # Password validation
