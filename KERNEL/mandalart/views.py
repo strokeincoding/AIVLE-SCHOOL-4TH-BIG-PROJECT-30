f9f07c4a0f50f08061701bcab44f5a1b42bb005b
taehyeong <steve181813@gmail.com>
2023-12-27 16:38:40 +0900

    temperature 수정

--- KERNEL/mandalart/views.py
+++ KERNEL/mandalart/views.py
@@ -10,9 +10,10 @@ def chatGPT(ask):
     keywords = {}
     for a in ask:
     # mandal = f'{query}로/으로 취업하기 준비해야 할 기술 스택, 문제해결 능력, soft skill , 트렌드를  설명과 예시를 생략하고 간략하게 짧은 키워드로만 알려주고 각 키워드를 세부적인 8가지 설명과 예시를 생략하고 간략하게 짧은 키워드로 알려줘'
-        mandal = f'프론트엔드 개발자로/으로 취업하기 준비해야 할 {a} 8가지를 설명과 예시를 생략하고 간략하게 짧은 키워드로 예시처럼 알려줘 출력 형태 1.키워드1 / 2. 키워드2'
+        mandal = f'프론트엔드 개발자로/으로 취업하기 준비해야 할 {a} 8가지를 설명과 예시를 생략하고 간략하게 짧은 키워드로 예시처럼 알려줘 출력 형태 1.키워드1  2. 키워드2  3.키워드3 4.키워드4 5.키워드5 6.키워드6 7.키워드7 8.키워드8'
         completion = client.chat.completions.create(model="gpt-3.5-turbo",
-        messages = [{"role": "user", "content": mandal}])
+        messages = [{"role": "user", "content": mandal}],
+        temperature=0)
         #print(completion)
         #result = completion.choices[0].message.content
         content = completion.choices[0].message.content
