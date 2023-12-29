995f722ce97766564b21be1b51ac7f0aa70cdaf8
hyunjin <dhguswls5028@naver.com>
2023-12-29 15:03:13 +0900

    홈화면 꾸밈. Home.css파일도 생성함

--- front/src/pages/Home.js
+++ front/src/pages/Home.js
@@ -1,28 +1,86 @@
 import React from 'react';
 import Card from '../components/Card/Card';
 import Slider from '../components/Slider/Slider';
-// Array of image objects for the slider
-const sliderImages = [
-  { src: 'path-to-your-image1.png', alt: 'Description of image 1' },
-  { src: 'path-to-your-image2.png', alt: 'Description of image 2' },
-  { src: 'path-to-your-image3.png', alt: 'Description of image 3' },
-  { src: 'path-to-your-image3.png', alt: 'Description of image 4' },
-  // Add more images as needed
+import './Home.css'; // 이 줄을 추가하세요.
+import mandaratImage from '../assets/mandarat.png';
+import bannerImage from '../assets/banner-image.jpg'; // Replace with your actual banner image path
+import chatbotImage from '../assets/chatbotImage.png'; // Replace with your actual image path
+import employmentImage from '../assets/employmentImage.png'; // Replace with your actual image path
+import TeamMemberCard from '../components/TeamMemberCard/TeamMemberCard';
+import team1 from '../assets/team1.png'; // Replace with your actual image path
+import team2 from '../assets/team2.png'; // Replace with your actual image path
+import team3 from '../assets/team3.png'; // Replace with your actual image path
+import team4 from '../assets/team4.png'; // Replace with your actual image path
+import team5 from '../assets/team5.png'; // Replace with your actual image path
+import team6 from '../assets/team6.png'; // Replace with your actual image path
+import team7 from '../assets/team7.png'; // Replace with your actual image path
+
+// 배너 섹션 컴포넌트
+const BannerSection = () => {
+  return (
+    <div className="banner-section" style={{ backgroundImage: `url(${bannerImage})` }}>
+      <div className="banner-content">
+        <h1>상바오</h1>
+        <p>"초보 개발자에게 정보제공?"</p>
+        <p>추천 서비스 및 만다라트 생성 등을 통해 초보 개발자 길잡이 역할을 해줍니다.</p>
+        <button className="banner-button">스터디 바로가기</button>
+      </div>
+    </div>
+  );
+};
+
+const settings = {
+  dots: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  centerMode: true, // 가운데 카드를 중심으로 보여주는 옵션
+  centerPadding: '60px', // 가운데 카드 양 옆의 패딩 값
+};
+
+const teamMembers = [
+  // Include `name` in each member object
+  { src: team1, alt: '팀원1 이름', name: '팀원1', description: '팀원1 설명' },
+  { src: team2, alt: '팀원2 이름', name: '팀원2', description: '팀원2 설명' },
+  { src: team3, alt: '팀원3 이름', name: '팀원3', description: '팀원3 설명' },
+  { src: team4, alt: '팀원4 이름', name: '팀원4', description: '팀원4 설명' },
+  { src: team5, alt: '팀원5 이름', name: '팀원5', description: '팀원5 설명' },
+  { src: team6, alt: '팀원6 이름', name: '팀원6', description: '팀원6 설명' },
+  { src: team7, alt: '팀원7 이름', name: '팀원7', description: '팀원7 설명' },
+  // ... other team members
 ];
 
-const Home = () => {
-    return(
-      <div>
-        <Slider images={sliderImages} />
-          <div className="card-container">
-            <Card title="Card Title 1" content="Content for card 1" />
-            <Card title="Card Title 2" content="Content for card 2" />
-            <Card title="Card Title 2" content="Content for card 3" />
-            <Card title="Card Title 2" content="Content for card 4" />
-            {/* More cards as needed */}
-          </div>
-      </div>
-    );
-  };
   
+const Home = () => {
+  return (
+    <div>
+      <BannerSection bannerImage={'path-to-your-banner-image.jpg'} />
+      <div className="card-container">
+        <Card 
+          className="card" 
+          title="만다라트" 
+          text="하이" 
+          imagePath={mandaratImage} 
+        />
+        <Card 
+          className="card" 
+          title="챗봇" 
+          text="하이" 
+          imagePath={chatbotImage} 
+        />
+        <Card 
+          className="card" 
+          title="취업" 
+          text="하이" 
+          imagePath={employmentImage} 
+        />
+      </div>
+      <Slider {...settings} images={teamMembers.map(member => ({
+        content: <TeamMemberCard key={member.name} src={member.src} alt={member.alt} name={member.name} description={member.description} />
+      }))} />
+    </div>
+  );
+};
+
 export default Home;
