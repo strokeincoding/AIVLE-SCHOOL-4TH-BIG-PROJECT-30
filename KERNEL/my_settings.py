60e8efd26827ca0c9a6a66544f5729d6a39a29f4
taehyeong <steve181813@gmail.com>
2023-12-29 17:45:53 +0900

    change my_settings

--- KERNEL/my_settings.py
+++ KERNEL/my_settings.py
@@ -3,7 +3,7 @@ from openai import OpenAI
 DATABASES = { 
 	'default': { 
     	'ENGINE': 'django.db.backends.mysql', 
-        'NAME': 'test16', 
+        'NAME': 'big_project', 
         'USER': 'root', 
         'PASSWORD': 'aivle', 
         'HOST': '127.0.0.1', 
