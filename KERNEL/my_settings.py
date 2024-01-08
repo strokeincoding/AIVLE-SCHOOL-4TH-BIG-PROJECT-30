0e42fb4f2b77194b08ccfa3531a16c0b9f015d8b
strokeincoding <vcx0526@gmail.com>
2024-01-08 16:02:52 +0900

    파비콘 수정

--- KERNEL/my_settings.py
+++ KERNEL/my_settings.py
@@ -2,9 +2,9 @@
 DATABASES = { 
 	'default': { 
     	'ENGINE': 'django.db.backends.mysql', 
-        'NAME': 'new', 
+        'NAME': 'test12', 
         'USER': 'root', 
-        'PASSWORD': '1234', 
+        'PASSWORD': 'aivle', 
         'HOST': '127.0.0.1' , 
         'PORT': '3306', 
         'OPTIONS' : {
